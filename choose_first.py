6a6317c8ed01ce048aef74ff16a31897d256b7b5
Kamil Ruchaj <kamilruchaj@gmail.com>
2021-03-22 19:59:42 +0100

    return list players

--- choose_first.py
+++ choose_first.py
@@ -6,6 +6,9 @@ import random
 def choose_first():
     a = random.randint(0, 1)
     if a == 0:
-        return str('player1')
+        return list(('Player 1', 'Player 2'))
     elif a == 1: 
-        return str('player2')
+        return list(('Player 2', 'Player 1'))
+
+# Test
+# print(choose_first())        
