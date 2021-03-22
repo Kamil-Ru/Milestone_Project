73940a23504b2b754445e476d891518841f847c7
Kamil Ruchaj <kamilruchaj@gmail.com>
2021-03-22 23:20:54 +0100

    add replay.py

--- replay.py
+++ replay.py
@@ -1,8 +1,14 @@
 def replay():
-    
-    while chose 
-    chose = input('Do you want to play again?\ny - for yes\nn - for no')
+    loop = False
+    while loop == False:
+        chose = input('Do you want to play again?\nType: "y" - for yes\nType: "n" - for no\n')
+        if chose == 'y':
+            loop = True
+            return True
+        elif chose == 'n':
+            loop = True
+            return False
+        else:
+            loop = False
 
-
-
-    pass
+# print(replay())
